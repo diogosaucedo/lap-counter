34bed5d7531e48f1be49ad39a57baf49a033d8a7
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-14 13:42:06 -0400

    feat(app.tsx): add logic part of lap counter
    'App.tsx' contains increment, decrement and reset functions. It has hooks to manage the time and
    states of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
+import ShowLaps from './components/Lap Counter/Show Laps';
+import Button from './components/Form/Button';
+import ShowTime from './components/Lap Counter/Show Time';
+import { Divider } from './components/Data Display/Divider/styles';
 
 function App() {
+  const [numLaps, setNumLaps] = React.useState(0);
+  const [running, setRunning] = React.useState(false);
+  const [time, setTime] = React.useState(0);
+
+  React.useEffect(() => {
+    let timer: ReturnType<typeof setInterval>;
+
+    if (running) {
+      timer = setInterval(() => {
+        setTime((old) => old + 1);
+      }, 1000);
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [running]);
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  };
+
+  const increment = () => {
+    setNumLaps(numLaps + 1);
+  };
+
+  const decrement = () => {
+    if (numLaps > 0) {
+      setNumLaps(numLaps - 1);
+    }
+  };
+
+  const reset = () => {
+    setNumLaps(0);
+    setTime(0);
+  };
+
   return (
     <div className="App">
       <GlobalStyles />
-      <header>
-        <h1>Hello World!</h1>
-      </header>
+      <ShowLaps laps={numLaps} />
+      <Button onClick={decrement} width="32px" height="16px">
+        -
+      </Button>
+      <Button onClick={increment} width="32px" height="16px">
+        +
+      </Button>
+      {numLaps > 0 && <ShowTime time={Math.round(time / numLaps)} />}
+      <Divider />
+      <Button onClick={toggleRunning}>{running ? 'Pause' : 'Start'}</Button>
+      <Button onClick={reset}>Restart</Button>
     </div>
   );
 }
