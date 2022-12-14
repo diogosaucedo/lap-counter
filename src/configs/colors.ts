16e9533254db9970b92f3aacb9446e9f671dd49a
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-14 12:58:22 -0400

    feat(colors.ts): update colors

--- src/configs/colors.ts
+++ src/configs/colors.ts
@@ -1,2 +1,7 @@
-export const primaryColor = '#000000';
-export const secondaryColor = '#FFFFFF';
+export const primaryColor = '#5A81FA';
+export const secondaryColor = '#2C3D8F';
+export const black = '#1F1F1F';
+export const gray = '#6A6E83';
+export const lightGray = '#A8B1C3';
+export const veryLightGray = '#F2F5FF';
+export const white = '#F8F9FD';
